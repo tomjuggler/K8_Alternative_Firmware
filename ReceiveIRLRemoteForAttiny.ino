104b3782d1005ebb000e4974805571e110e6bdec
Tom Hastings <tomjuggler@gmail.com>
2017-09-22 11:56:45 +0200

    3 Signals working, refactored a bit, added all signal codes
    Signed-off-by: Tom Hastings <tomjuggler@gmail.com>

--- ReceiveIRLRemoteForAttiny.ino
+++ ReceiveIRLRemoteForAttiny.ino
@@ -47,7 +47,9 @@ CNec IRLremote;
 //#define pinLed 3
 
 boolean ready = false;
-
+//these are the exact same pins as K8:
+//1, 3, 4
+//only difference is (maybe) the colours!!!!!
 int blueLed = 1; //on right
 int greenLed = 3; //middle
 int redLed = 4; //left
@@ -56,20 +58,47 @@ int delayTime = 250;
 int selection = 0;
 int maxSelection = 3;
 
+////////////////////////HEX codes:///////////////////////////
+uint8_t greenHEX = 0x9;
+uint8_t redHEX = 0x8;
+uint8_t blueHEX = 0xA;
+uint8_t yellowHEX = 0xB;
+uint8_t cyanHEX = 0xD;
+uint8_t magentaHEX = 0xC;
+uint8_t whiteHEX = 0xE;
+uint8_t fadeHEX = 0xF;
+uint8_t strobeplusHEX = 0x15;
+uint8_t RGBStrobeHEX = 0x14;
+uint8_t rainbowHEX = 0x16;
+uint8_t halfstrobeHEX = 0x17;
+uint8_t BGStrobeHEX = 0x19;
+uint8_t GRStrobeHEX = 0x18;
+uint8_t onHEX = 0x6;
+uint8_t offHEX = 0x7;
+uint8_t nextHEX = 0x4;
+uint8_t previousHEX = 0x5;
+uint8_t demoHEX = 0x1A;
+uint8_t extraHEX1 = 0x1B;
+uint8_t extraHEX2 = 0x11;
+uint8_t extraHEX3 = 0x10;
+uint8_t extraHEX4 = 0x12;
+uint8_t extraHEX5 = 0x13;
+
+
 void setup()
 {
-pinMode(blueLed, OUTPUT); 
-pinMode(greenLed, OUTPUT); 
-pinMode(redLed, OUTPUT); 
-//  Serial.begin(115200);
-//  Serial.println(F("Startup"));
-//GIMSK = 0b00100000;    // turns on pin change interrupts
-//    PCMSK = 0b00010011;    // turn on interrupts on pins PB0, PB1, &amp;amp; PB4
-//    sei();
+  pinMode(blueLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  //  Serial.begin(115200);
+  //  Serial.println(F("Startup"));
+  //GIMSK = 0b00100000;    // turns on pin change interrupts
+  //    PCMSK = 0b00010011;    // turn on interrupts on pins PB0, PB1, &amp;amp; PB4
+  //    sei();
   // Set LED to output
-//  pinMode(pinLed, OUTPUT);
-//  digitalWrite(pinLed, HIGH);
-Red();
+  //  pinMode(pinLed, OUTPUT);
+  //  digitalWrite(pinLed, HIGH);
+  Red();
   delay(delayTime);
   Off();
   delay(delayTime);
@@ -81,185 +110,189 @@ Red();
   delay(delayTime);
   Off();
   delay(delayTime);
-//digitalWrite(pinLed, LOW);
+  //digitalWrite(pinLed, LOW);
   // Start reading the remote. PinInterrupt or PinChangeInterrupt* will automatically be selected
-//  if (!IRLremote.begin(pinIR)){
+  //  if (!IRLremote.begin(pinIR)){
   if (!IRLremote.begin(pinIR))
-{
+  {
+  }
 }
-    }
 
 void loop()
 {
- 
-  
-  if(ready){
+
+
+  if (ready) {
     Off();
     //not using?
-   selection++;
-   if(selection >= maxSelection){
-    selection = 0;
-   }
+    selection++;
+    if (selection >= maxSelection) {
+      selection = 0;
+    }
   }
- /*
-  switch(selection){
-      case 0: {
-        Red();
-      }
-      case 1: {
-        Green();
-      }
-      case 2: {
-        Blue();
-      }
-      default:{
-        Off(); //never gets here
-      }
-   }
+  /*
+    switch(selection){
+       case 0: {
+         Red();
+       }
+       case 1: {
+         Green();
+       }
+       case 2: {
+         Blue();
+       }
+       default:{
+         Off(); //never gets here
+       }
+    }
   */
   // Check if we are currently receiving data
   //if (!IRLremote.receiving()) {
-    // Run code that disables interrupts, such as some led strips
+  // Run code that disables interrupts, such as some led strips
   //}
 
   // Check if new IR protocol data is available
   if (IRLremote.available())
   {
-//    Off();
-//    ready = true;
+    //    Off();
+    //    ready = true;
     // Light Led
-//    digitalWrite(pinLed, HIGH);
+    //    digitalWrite(pinLed, HIGH);
 
     // Get the new data from the remote
     auto data = IRLremote.read();
 
     // Print the protocol data
-//    Serial.print(F("Address: 0x"));
-//    Serial.println(data.address, HEX);
-//    Serial.print(F("Command: 0x"));
-//    Serial.println(data.command, HEX);
-//    Serial.println();
+    //    Serial.print(F("Address: 0x"));
+    //    Serial.println(data.address, HEX);
+    //    Serial.print(F("Command: 0x"));
+    //    Serial.println(data.command, HEX);
+    //    Serial.println();
 
     // Turn Led off after printing the data
-//    digitalWrite(pinLed, LOW);
-if(data.command == 9){
-  Off();
-    Green();
-    }
-    else if(data.command == 8){
-      Off();
-    Red();
-    }
-    else if(data.command == 15){ //need to decode letters 15 not working...      Off();
+    //    digitalWrite(pinLed, LOW);
+    testCommand(data.command);
+
+  }
+}
+
+void testCommand(uint8_t inSignal) {
+  if (inSignal == greenHEX) { //Green
+    Off();
     Blue();
-    }
+  } else if (inSignal == redHEX) { //Red
+    Off();
+    Red();
+  } else if (inSignal == blueHEX) { //Blue //need to decode letters 15 not working...      Off();
+    Green();
   }
 }
 
 //0a
-    void Red() {
-    Off();
-    digitalWrite(redLed, HIGH);
-//    mySerial.println("Red");
-    }      
+void Red() {
+  Off();
+  digitalWrite(redLed, HIGH);
+  
+}
 //1b green
-    void Green() {
-    Off();
-    digitalWrite(greenLed, HIGH);
-//    mySerial.println("Green");
-    }
+void Green() {
+  Off();
+  digitalWrite(greenLed, HIGH);
+ 
+}
 //2c blue
-    void Blue() {
-    Off();
-    digitalWrite(blueLed, HIGH);
-//    mySerial.println("Blue");
-    }
+void Blue() {
+  Off();
+  digitalWrite(blueLed, HIGH);
+ 
+}
 //3d Yellow
-    void Yellow() {
-   Off();
-   
-   // mySerial.println("d");
-    }
+void Yellow() {
+  Off();
+
+ 
+}
 //4e Cyan
-    void Cyan() {
-    Off();
-    
-   // mySerial.println("e");
-    }
+void Cyan() {
+  Off();
+
+ 
+}
 //5f Magenta
-    void Magenta() {
-   
-   // mySerial.println("f");
-    }
+void Magenta() {
+  Off();
+
+
+}
 //6g White
-    void White() {
-   
- //   mySerial.println("g");
-    }
+void White() {
+
+ 
+}
 //7h Fade
-    void Fade() {
-    
-   // mySerial.println("h");
-    }
+void Fade() {
+
+ 
+}
 //8i Strobe+
-    void Strobeplus() {
-    
-  //  mySerial.println("i");
-    }
+void Strobeplus() {
+
+ 
+}
 //9j RGBStrobe
-    void RGBStrobe() {
-    
-   // mySerial.println("j");
-    }
-//10k Rainbow
-    void Rainbow() {
-    
-  //  mySerial.println("k");
-    }
-//11L Halfstrobe
-    void Halfstrobe() {
-    
-  //  mySerial.println("l");
-    }
-//12m BGStrobe
-    void BGStrobe() {
-    
-  //  mySerial.println("m");
-    }
-//13n GRStrobe
-    void GRStrobe() {
-    
-  //  mySerial.println("n");
-    }
-//14oOff
-    void Off() {
-digitalWrite(greenLed, LOW);
-   digitalWrite(blueLed, LOW);
-   digitalWrite(redLed, LOW);
-   
-  //  mySerial.println("o");
-    }
-//15p Next
-    void Next() {
-    
-   // mySerial.println("p");
-    }
-//16q Demo
-    void Demo() {
-    
-   // mySerial.println("q");
-    }
-//17r Previous
-    void Previous() {
-    
-  //  mySerial.println("r");
-    }
-
-
-
-
+void RGBStrobe() {
 
   
+}
+//10k Rainbow
+void Rainbow() {
+
+  
+}
+//11L Halfstrobe
+void Halfstrobe() {
+
+  
+}
+//12m BGStrobe
+void BGStrobe() {
+
+  
+}
+//13n GRStrobe
+void GRStrobe() {
+
+  
+}
+//14oOff
+void Off() {
+  digitalWrite(greenLed, LOW);
+  digitalWrite(blueLed, LOW);
+  digitalWrite(redLed, LOW);
+
+  
+}
+//15p Next
+void Next() {
+
+  
+}
+//16q Demo
+void Demo() {
+
+  
+}
+//17r Previous
+void Previous() {
+
+  
+}
+
+
+
+
+
+
 
 
 
